bbfa0eb1477c058fa97573ba9b3f486c3228b16c
Rui Dantas <ruidantas55@gmail.com>
2022-10-23 20:30:46 +0000

    removing items from arrays

---
+++ without.js
@@ -0,0 +1,30 @@
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log("Arrays are equal!")
+  }else {
+    console.log("Arrays are not equal!")
+  }
+}
+
+const eqArrays = function (arr1, arr2) {
+  for(let i  = 0; i < arr1.length; i++) {
+    if(arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const without = function(arr1,arr2) {
+  let outputArray = []
+  for (let i = 0; i < arr1.length; i++){
+    if( !arr2.includes(arr1[i])){
+      outputArray.push(arr1[i])
+    }
+  }
+  
+  return outputArray
+}
+
+console.log(without([1, 2, 3], [1])) // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
