a76bc617f912f0b510f7d4ffc3bb7be16c5b8854
Rui Dantas <ruidantas55@gmail.com>
2022-10-29 21:07:26 +0000

    Find the first key that matches

---
+++ findKey.js
@@ -0,0 +1,31 @@
+const assertEqual  = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed:, ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`Assertion Failed:, ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKey = (obj, callback) => {
+  for (const element in obj) {
+    if(callback(obj[element])) {
+      return element;
+    }
+  }
+  return false;
+}
+
+
+
+const key = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)// => "noma"
+
+console.log(assertEqual(key), true)
+
