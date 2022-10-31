94245c574397146926e143c5ac45806c3a718472
Rui Dantas <ruidantas55@gmail.com>
2022-10-31 16:21:42 +0000

    getting familiar with the map function

---
+++ map.js
@@ -0,0 +1,36 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+
+const eqArrays = function (arr1, arr2) {
+  for(let i  = 0; i < arr1.length; i++) {
+    if(arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log("Arrays are equal!")
+  }else {
+    console.log("Arrays are not equal!")
+  }
+}
+
+
+
+const map = function(array, callback) {
+  // empty for now :)
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+
+assertArraysEqual(results1, words)
