6f6683813f11a54a995b80a86d0835e16b86f5d3
Rui Dantas <ruidantas55@gmail.com>
2022-11-10 17:03:23 +0000

    tail test with updated code

--- assertEqual.js
+++ assertEqual.js
@@ -1,28 +1,3 @@
-/* FUNCTION IMPLEMENTATION
-const sum = function(a, b) {
-  return a + b;
-}
-
-// TEST CODE
-console.assert(sum(1, 2) === 3);
-console.assert(sum(1, 20) === 3); // bad / incorrect assertion, and we see it fail!
-
-//This would allow us to catch bugs in our code. Let's say we had a buggy sum function instead:
-
-------------------------------------------------------------------------------------------------------------
-
-
-// FUNCTION IMPLEMENTATION
-const sumBuggy = function(a, b) {
-  return a * b;
-}
-
-// TEST CODE
-console.assert(sumBuggy(1, 2) === 3); // fails, because bug!
-
-------------------------------------------------------------------------------------------------------------
-*/
-
 const assertEqual  = function(actual, expected) {
   if (actual === expected) {
     console.log(`Assertion Passed:, ${actual} === ${expected}`);
@@ -31,5 +6,5 @@ const assertEqual  = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1,1);
+
+module.exports = assertEqual;
