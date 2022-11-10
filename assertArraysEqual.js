464d4dde411b250689dbb6984af1753ee1e1b455
Rui Dantas <ruidantas55@gmail.com>
2022-11-10 18:04:09 +0000

    Assert Equal Arrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,28 @@
-const eqArrays = function (arr1, arr2) {
-  for(let i  = 0; i < arr1.length; i++) {
-    if(arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
+// const eqArrays = function(arr1, arr2) {
+//   for (let i  = 0; i < arr1.length; i++) {
+//     if (arr1[i] !== arr2[i]) {
+//       return false;
+//     } else {
+//       for (let i = 0; i < arr1.length; i++) {
+//         if (arr1[i] !== arr2[i]) {
+//           return false;
+//         }
+//       }
+//       return true;
+//     }
+//   }
+// };
 
+const eqArrays = require('../lotide/eqArrays')
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
-    console.log("Arrays are equal!")
-  }else {
-    console.log("Arrays are not equal!")
+    console.log(`✅✅✅ Assertion Passed: [${arr1}] is equal to [${arr2}]`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: [${arr1}] is different than [${arr2}]`);
   }
-}
+};
 
-assertArraysEqual([1, 2, 3],[1, 2, 3])
-assertArraysEqual([1, 2, 3],[1, 2, 2])
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3])
+module.exports = assertArraysEqual;
+
+
