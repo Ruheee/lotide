82593d2aa326ab4a5121a558a568dfa5c7d91b49
Rui Dantas <ruidantas55@gmail.com>
2022-11-12 22:22:34 +0000

    Refactor code

--- letterPositions.js
+++ letterPositions.js
@@ -1,54 +1,15 @@
-/**Create a for loop that itterates through the string
- * 
- */
-
 const letterPositions = function(sentence) {
   const results = {};
-  
- 
-  const temp = sentence.split("")
-  temp.forEach((char,idx) => {
-    if (results[char]) { //results = {"c": [0, 5]}
-      results[char].push(idx)
-    } else if (char !== " ") {
-      results[char] = [idx]
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== ' ') {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
     }
-    
-  })
+  }
   return results;
-}
-  
- // logic to update results here
-  // how i was doing it originally, Was on the right path. 
-  // for (const character of sentence) {
-  //   // for( let i = 0; i < character.length; i++){
-  //   //   if (results[character]) {
-  //   //     results[character] += 1;
-  //   //   } else if (character !== " ") {
-  //   //     results[character] = 1;
-  //   //   } else if (character === [i]){
-  //   //     results[character] += [i]
-  //   //   }
-  //   // }
-    
-  
-  // }
-const eqArrays = function (arr1, arr2) {
-  for(let i  = 0; i < arr1.length; i++) {
-    if(arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
+};
 
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log("Arrays are equal!")
-  }else {
-    console.log("Arrays are not equal!")
-  }
-}
-
-
-assertArraysEqual(letterPositions("Coding is kicking my butt").i, [3, 7, 11, 14]);
+module.exports = letterPositions;
